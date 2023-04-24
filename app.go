e811902f91d24bb1b1f0e994039d8742ba11cc86
Marsel <marselbisengaliev1@gmail.com>
2023-04-24 20:41:13 +0500

    Добавление ссылок на сайты. Работа с Hyperlink

--- app.go
+++ app.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -11,49 +12,20 @@ import (
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Calculator")
-	w.Resize(fyne.NewSize(300, 500))
+	w := a.NewWindow("Telegram account")
+	w.Resize(fyne.NewSize(300, 400))
 
-	title := widget.NewLabel("Order placement")
+	url, err := url.Parse("https://t.me/marsel_bisengaliev")
 
-	nameLabel := widget.NewLabel("Your name:")
-	name:= widget.NewEntry()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 
-	foodLabel := widget.NewLabel("Choice a food for order")
-	food := widget.NewCheckGroup([]string{"Pizza", "Cake", "Nuggets", "Burger", "Coca-Cola"}, func([]string) {})
-
-	genderLabel := widget.NewLabel("Your gender:")
-	gender := widget.NewRadioGroup([]string{"Male", "Female"}, func(s string) {})
-
-	result := widget.NewLabel("")
-
-	btn := widget.NewButton("Order", func() {
-		username := name.Text
-		selectedFoods := food.Selected
-		selectedGender := gender.Selected
-
-		parsedSelectedFoods := ""
-		for i, foodEl := range selectedFoods {
-			if i + 1 == len(selectedFoods) {
-				parsedSelectedFoods += foodEl
-				break
-			}
-			parsedSelectedFoods += foodEl + ", "
-		}
-
-		result.SetText(fmt.Sprintf("%s customer %s ordered: %s.", selectedGender, username, parsedSelectedFoods))
-	})
+	link := widget.NewHyperlink("Write me!", url)
 
 	w.SetContent(container.NewVBox(
-		title,
-		nameLabel,
-		name,
-		foodLabel,
-		food,
-		genderLabel,
-		gender,
-		btn,
-		result,
+		link,
 	))
 
 	w.ShowAndRun()
