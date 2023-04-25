423201c69d2b274cb704905ff23673d2aa00c82d
Marsel <marselbisengaliev1@gmail.com>
2023-04-25 20:31:49 +0500

    Изменяю размеры и расположения элементов на окне

--- app.go
+++ app.go
@@ -10,18 +10,71 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
+func createEntry(
+	size fyne.Size,
+	pos fyne.Position,
+	placeholder string,
+	isPassword bool,
+	isMultiline bool,
+) *widget.Entry {
+	entry := widget.NewEntry()
+
+	if isPassword {
+		entry = widget.NewPasswordEntry()
+	} else if isMultiline {
+		entry = widget.NewMultiLineEntry()
+	}
+
+	entry.Resize(size)
+	entry.Move(pos)
+	entry.SetPlaceHolder(placeholder)
+
+	return entry
+}
+
 func main() {
 	a := app.New()
 	w := a.NewWindow("Marsel App")
-	w.Resize(fyne.NewSize(250, 250))
+	w.Resize(fyne.NewSize(400, 400))
 
-	label := widget.NewLabel("Notebook")
+	label := widget.NewLabel("REGISTRATION")
+	label.TextStyle = fyne.TextStyle{Bold: true}
+	label.Move(fyne.NewPos(140, 3))
 
-	entry := widget.NewMultiLineEntry()
-	entry.SetPlaceHolder("Enter here...")
+	username := createEntry(
+		fyne.NewSize(300, 40),
+		fyne.NewPos(50, 50),
+		"Username",
+		false,
+		false,
+	)
 
-	btn := widget.NewButton("Save", func() {
-		file, err := os.Create("info.txt")
+	email := createEntry(
+		fyne.NewSize(300, 40),
+		fyne.NewPos(50, 110),
+		"Email",
+		false,
+		false,
+	)
+
+	password := createEntry(
+		fyne.NewSize(300, 40),
+		fyne.NewPos(50, 170),
+		"Password",
+		true,
+		false,
+	)
+
+	description := createEntry(
+		fyne.NewSize(300, 80),
+		fyne.NewPos(50, 230),
+		"Description",
+		false,
+		true,
+	)
+
+	submit := widget.NewButton("SIGN UP", func() {
+		file, err := os.Create("users.txt")
 
 		if err != nil {
 			fmt.Println(err.Error())
@@ -30,12 +83,29 @@ func main() {
 
 		defer file.Close()
 
-		file.WriteString(entry.Text)
+		data := fmt.Sprintf(
+			"-Username: %s\n-Password: %s\n-Email: %s\n-Description: %s\n",
+			username.Text,
+			password.Text,
+			email.Text,
+			description.Text,
+		)
+
+		file.WriteString(data)
 	})
+	submit.Resize(fyne.NewSize(200, 40))
+	submit.Move(fyne.NewPos(100, 340))
 
-	content := container.NewVBox(label, entry, btn)
+	container := container.NewWithoutLayout(
+		label,
+		username,
+		email,
+		password,
+		description,
+		submit,
+	)
 
-	w.SetContent(content)
+	w.SetContent(container)
 
 	w.ShowAndRun()
 }
