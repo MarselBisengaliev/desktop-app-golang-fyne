b8b3756fa578df7ca4ed2adf1a8ef8ac05a6e6b3
Marsel <marselbisengaliev1@gmail.com>
2023-04-25 21:08:51 +0500

    Создаю главное меню для приложения, а также дочерние меню и кнопки

--- app.go
+++ app.go
@@ -6,106 +6,26 @@ import (
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
 )
-
-func createEntry(
-	size fyne.Size,
-	pos fyne.Position,
-	placeholder string,
-	isPassword bool,
-	isMultiline bool,
-) *widget.Entry {
-	entry := widget.NewEntry()
-
-	if isPassword {
-		entry = widget.NewPasswordEntry()
-	} else if isMultiline {
-		entry = widget.NewMultiLineEntry()
-	}
-
-	entry.Resize(size)
-	entry.Move(pos)
-	entry.SetPlaceHolder(placeholder)
-
-	return entry
-}
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Marsel App")
 	w.Resize(fyne.NewSize(400, 400))
 
-	label := widget.NewLabel("REGISTRATION")
-	label.TextStyle = fyne.TextStyle{Bold: true}
-	label.Move(fyne.NewPos(140, 3))
+	fileItem1 := fyne.NewMenuItem("New File", func() { os.Create("file.txt") })
+	fileItem2 := fyne.NewMenuItem("Save", func() { fmt.Println("File Saved!") })
 
-	username := createEntry(
-		fyne.NewSize(300, 40),
-		fyne.NewPos(50, 50),
-		"Username",
-		false,
-		false,
-	)
+	menu1 := fyne.NewMenu("File", fileItem1, fileItem2)
 
-	email := createEntry(
-		fyne.NewSize(300, 40),
-		fyne.NewPos(50, 110),
-		"Email",
-		false,
-		false,
-	)
+	actionsItem1 := fyne.NewMenuItem("Hello", func() { fmt.Println("Hello from menu") })
+	actionsItem2 := fyne.NewMenuItem("Bye", func() { fmt.Println("Bye bye") })
+	actionsItem3 := fyne.NewMenuItem("Button", func() { fmt.Println("Clicked") })
 
-	password := createEntry(
-		fyne.NewSize(300, 40),
-		fyne.NewPos(50, 170),
-		"Password",
-		true,
-		false,
-	)
+	menu2 := fyne.NewMenu("Actions", actionsItem1, actionsItem2, actionsItem3)
 
-	description := createEntry(
-		fyne.NewSize(300, 80),
-		fyne.NewPos(50, 230),
-		"Description",
-		false,
-		true,
-	)
-
-	submit := widget.NewButton("SIGN UP", func() {
-		file, err := os.Create("users.txt")
-
-		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(1)
-		}
-
-		defer file.Close()
-
-		data := fmt.Sprintf(
-			"-Username: %s\n-Password: %s\n-Email: %s\n-Description: %s\n",
-			username.Text,
-			password.Text,
-			email.Text,
-			description.Text,
-		)
-
-		file.WriteString(data)
-	})
-	submit.Resize(fyne.NewSize(200, 40))
-	submit.Move(fyne.NewPos(100, 340))
-
-	container := container.NewWithoutLayout(
-		label,
-		username,
-		email,
-		password,
-		description,
-		submit,
-	)
-
-	w.SetContent(container)
+	mainMenu := fyne.NewMainMenu(menu1, menu2)
+	w.SetMainMenu(mainMenu)
 
 	w.ShowAndRun()
 }
