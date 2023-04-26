e847f4bf60ce2fd0d6be2bedbee33a1205e858c9
Marsel <marselbisengaliev1@gmail.com>
2023-04-26 15:30:58 +0500

    Создаю и работаю с элементами HSplit и VSplit, разделители.

--- app.go
+++ app.go
@@ -3,59 +3,37 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
+
+func createText() *widget.Label {
+	text := widget.NewLabel("Irure occaecat voluptate consectetur exercitation veniam quis cillum laborum. Eiusmod laboris officia qui mollit nulla ipsum. Commodo enim eiusmod Lorem tempor nulla occaecat duis exercitation. Proident enim voluptate exercitation id.")
+	text.Wrapping = fyne.TextWrapBreak
+
+	return text
+}
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Marsel App")
 	w.Resize(fyne.NewSize(400, 400))
 
-	card := widget.NewCard(
-		"Marsel GROUP",
-		"Read more about the user accept",
-		widget.NewAccordion(
-			widget.NewAccordionItem(
-				"Pargrapgh 1",
-				widget.NewAccordion(
-					widget.NewAccordionItem(
-						"Point 1",
-						widget.NewLabel("Info about point 1"),
-					),
-					widget.NewAccordionItem(
-						"Point 2",
-						widget.NewLabel("Info about point 2"),
-					),
-					widget.NewAccordionItem(
-						"Point 3",
-						widget.NewLabel("Info about point 3"),
-					),
-				),
-			),
-			widget.NewAccordionItem(
-				"Pargrapgh 2",
-				widget.NewAccordion(
-					widget.NewAccordionItem(
-						"Point 1",
-						widget.NewLabel("Info about point 1"),
-					),
-					widget.NewAccordionItem(
-						"Point 2",
-						widget.NewLabel("Info about point 2"),
-					),
-					widget.NewAccordionItem(
-						"Point 3",
-						widget.NewLabel("Info about point 3"),
-					),
-				),
-			),
-			widget.NewAccordionItem(
-				"More information",
-				widget.NewLabel("More information for user"),
-			),
-		),
-	)
+	title := widget.NewLabel("ORTHOGRAPHY")
 
-	w.SetContent(card)
+	rightLabel := widget.NewLabel("RIGHT")
+	wrongLabel := widget.NewLabel("WRONG")
+
+	rightText := createText()
+	wrongText := createText()
+
+	rightBox := container.NewVBox(rightLabel, rightText)
+	wrongBox := container.NewVBox(wrongLabel, wrongText)
+
+	split := container.NewHSplit(rightBox, wrongBox)
+
+	content := container.NewVBox(title, split)
+
+	w.SetContent(content)
 	w.ShowAndRun()
 }
