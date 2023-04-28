28f3a439b577faae5459ee1fb165391aa1f3a28c
Marsel <marselbisengaliev1@gmail.com>
2023-04-28 15:23:04 +0500

    Работаю с Select Dropdown, кастомизация, SelectEntry

--- app.go
+++ app.go
@@ -1,50 +1,41 @@
 package main
 
 import (
-	"image/color"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
-
-func createRectangle(color color.RGBA) *canvas.Rectangle {
-	rectangle := canvas.NewRectangle(color)
-	rectangle.SetMinSize(fyne.NewSize(400, 400))
-
-	return rectangle
-}
-
-func createImage(file string) *canvas.Image {
-	image := canvas.NewImageFromFile(file)
-	image.SetMinSize(fyne.NewSize(400, 400))
-
-	return image
-}
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Marsel App")
 	w.Resize(fyne.NewSize(400, 400))
 
-	img1 := createImage("icon.png")
-	img2 := createImage("icon2.jpg")
+	label := widget.NewLabel("")
 
-	rec1 := createRectangle(color.RGBA{79, 32, 106, 1})
-	rec2 := createRectangle(color.RGBA{157, 51, 51, 1})
-	rec3 := createRectangle(color.RGBA{118, 183, 39, 1})
-
-	scroll := container.NewVScroll(
-		container.NewVBox(
-			img1,
-			img2,
-			rec1,
-			rec2,
-			rec3,
-		),
+	sel := widget.NewSelectEntry(
+		[]string{
+			"Option 1",
+			"Option 2",
+			"Option 3",
+			"Option 4",
+			"Option 5",
+		},
 	)
 
-	w.SetContent(scroll)
+	sel.PlaceHolder = "Choose one or enter custom option"
+
+	btn := widget.NewButton("Get Data", func() {
+		label.SetText("You chose " + sel.Text)
+	})
+
+	w.SetContent(
+		container.NewVBox(
+			sel,
+			btn,
+			label,
+		),
+	)
 	w.ShowAndRun()
 }
