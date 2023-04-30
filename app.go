dad393eb39a25634482f31bc4bcffdd121462e7f
Marsel <marselbisengaliev1@gmail.com>
2023-04-30 21:19:00 +0500

    Контейнер AppTabs, расположение вкладок, иконки вкладок

--- app.go
+++ app.go
@@ -6,14 +6,14 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
-func createCenterLayout(widget fyne.Widget) *fyne.Container {
-	c := container.New(
-		layout.NewCenterLayout(),
-		widget,
+func createContainer(text string) *fyne.Container {
+	c := container.NewVBox(
+		widget.NewLabel(text),
+		widget.NewEntry(),
+		widget.NewButton("CLICK", nil),
 	)
 
 	return c
@@ -22,23 +22,30 @@ func createCenterLayout(widget fyne.Widget) *fyne.Container {
 func main() {
 	a := app.New()
 	w := a.NewWindow("Marsel App")
+	w.Resize(fyne.NewSize(500, 500))
 
-	l := widget.NewLabel("Label")
-	e := widget.NewEntry()
-	b := widget.NewButton("Button", func() {
-		fmt.Println(e.Text)
-	})
+	img, err := fyne.LoadResourceFromPath("./icon.png")
 
-	cl := createCenterLayout(l)
-	ce := createCenterLayout(e)
-	cb := createCenterLayout(b)
+	if err != nil {
+		fmt.Println("Img has not been found")
+		return
+	}
 
-	w.SetContent(
-		container.NewVBox(
-			cl,
-			ce,
-			cb,
-		),
+	c1 := createContainer("Marsel")
+	c2 := createContainer("John")
+	c3 := createContainer("Kate")
+	c4 := createContainer("Andrew")
+
+	t := container.NewAppTabs(
+		container.NewTabItemWithIcon("Tab 1", img, c1),
+		container.NewTabItemWithIcon("Tab 2", img, c2),
+		container.NewTabItemWithIcon("Tab 3", img, c3),	
+		container.NewTabItemWithIcon("Tab 4", img, c4),	
 	)
-	w.ShowAndRun()
+
+	t.SetTabLocation(container.TabLocationTop)
+
+	w.SetContent(t)
+	w.Show()
+	a.Run()
 }
