75e291dc19cbf3e6dd8536d4f190c9177e83e4d9
Marsel <marselbisengaliev1@gmail.com>
2023-05-03 16:35:04 +0500

    Наполняю таблицу данными разных типов

--- app.go
+++ app.go
@@ -1,31 +1,46 @@
 package main
 
 import (
+	"fmt"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
 )
+
+type Student struct {
+	marks []int
+	name  string
+}
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Marsel App")
 	w.Resize(fyne.NewSize(500, 500))
 
-	evaluations := [][]string{
-		{"John", "12", "12", "12"},
-		{"Andrew", "11", "11", "11"},
-		{"Kate", "10", "10", "10"},
+	students := []Student{
+		{marks: []int{9, 9, 9}, name: "Kate"},
+		{marks: []int{10, 10, 10}, name: "John"},
+		{marks: []int{11, 11, 11}, name: "Andrew"},
+		{marks: []int{12, 8, 10}, name: "Vlad"},
 	}
 
 	table := widget.NewTable(
 		func() (int, int) {
-			return len(evaluations), len(evaluations[0])
+			return len(students), len(students[0].marks) + 1
 		},
+		
 		func() fyne.CanvasObject {
 			return widget.NewLabel("Default text")
 		},
+		
 		func(tci widget.TableCellID, co fyne.CanvasObject) {
-			co.(*widget.Label).SetText(evaluations[tci.Row][tci.Col])
+			if tci.Col == 0 {
+				co.(*widget.Label).SetText(students[tci.Row].name)
+				return
+			}
+
+			co.(*widget.Label).SetText(fmt.Sprint(students[tci.Row].marks[tci.Col-1]))
 		},
 	)
 
